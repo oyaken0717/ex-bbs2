fd6aea89b006d9dc60f9d43d90f24503fd0daaca
KenjiOyamada <oyaken0717@yahoo.co.jp>
2020-02-13 01:31:49 +0900

    【add CommentRepositoryのinsertメソッドを追加する。

--- src/main/java/com/example/repository/CommentRepository.java
+++ src/main/java/com/example/repository/CommentRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
@@ -31,5 +32,11 @@ public class CommentRepository {
 		SqlParameterSource param = new MapSqlParameterSource().addValue("articleId", articleId);
 		List<Comment> commentList = template.query(sql, param, COMMENT_ROW_MAPPER);
 		return commentList;
+	}
+	
+	public void insert(Comment comment) {
+		String sql = "INSERT INTO comments (name, content, article_id) VALUES (:name,:content,:articleId)";
+		SqlParameterSource param = new BeanPropertySqlParameterSource(comment);
+		template.update(sql, param);
 	}
 }
