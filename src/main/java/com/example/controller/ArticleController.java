d81e58393fce3eba95b3c67b325f03a0af2ce7f6
KenjiOyamada <oyaken0717@yahoo.co.jp>
2020-02-12 15:45:20 +0900

    【modify】indexメソッド内で記事の全件取得をできるように変更

--- src/main/java/com/example/controller/ArticleController.java
+++ src/main/java/com/example/controller/ArticleController.java
@@ -1,16 +1,26 @@
 package com.example.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.Article;
+import com.example.repository.ArticleRepository;
 
 @Controller
 @RequestMapping("/article")
 public class ArticleController {
 
+	@Autowired
+	private ArticleRepository articleRepository;
+	
 	@RequestMapping("")
-	public String index() {
-		
+	public String index(Model model) {
+		List<Article> articleList = articleRepository.findAll();
+		model.addAttribute("articleList",articleList);
 		return "index";
 	}
 	
