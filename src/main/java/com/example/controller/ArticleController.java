9f21cbc2dd3f0f7d27fefc9b3acfa30566a2ac3c
KenjiOyamada <oyaken0717@yahoo.co.jp>
2020-02-12 13:45:26 +0900

    【add】receiveFormメソッドの実装

--- src/main/java/com/example/controller/ArticleController.java
+++ src/main/java/com/example/controller/ArticleController.java
@@ -1,14 +1,20 @@
 package com.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/board")
-public class BoardController {
+@RequestMapping("/article")
+public class ArticleController {
 
 	@RequestMapping("")
 	public String index() {
 		return "index";
+	}
+	
+	@RequestMapping("receive-form")
+	public String receiveForm(Model model) {
+		return "result";
 	}
 }
