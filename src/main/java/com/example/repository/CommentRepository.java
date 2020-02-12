bbf162cb418c34583e26b901e8b0e8e8c867351c
KenjiOyamada <oyaken0717@yahoo.co.jp>
2020-02-12 20:34:36 +0900

    【add】CommentRepositoryのfindByArticleId()の実装

--- src/main/java/com/example/repository/CommentRepository.java
+++ src/main/java/com/example/repository/CommentRepository.java
@@ -1,8 +1,12 @@
 package com.example.repository;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
 import com.example.domain.Comment;
@@ -23,4 +27,10 @@ public class CommentRepository {
 	};
 	
 	
+	public List<Comment> findByArticleId(Integer articleId) {
+		String sql ="SELECT id, name, content, article_id FROM comments WHERE article_id = :articleId";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("articleId", articleId);
+		List<Comment> commentList = template.query(sql, param, COMMENT_ROW_MAPPER);
+		return commentList;
+	}
 }
